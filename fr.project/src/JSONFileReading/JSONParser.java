18cf807f08d04115f90885b4698a9c4782fe2e69
Noah <noah.remerand@etu.univ-tours.fr>
2021-11-25 11:27:47 +0100

    Json parser to String

--- fr.project/src/JSONFileReading/JSONParser.java
+++ fr.project/src/JSONFileReading/JSONParser.java
@@ -12,6 +12,8 @@ public class JSONParser {
 
     public static void main(String[] args){
 
+        String url,password, dbName, login;
+
         //Not usable for now need to finish it at one point though.
 
         try{
@@ -20,10 +22,25 @@ public class JSONParser {
             Gson gson = new Gson();
 
             //Create a Reader
-            Reader reader = Files.newBufferedReader(Path.of("/Desktop/ProjetPOOV/bdd.json"));
+            Reader reader = Files.newBufferedReader(Path.of("/bdd.json"));
 
             //Mapping JSON File
             Map<?, ?> map = gson.fromJson(reader, Map.class);
+
+            //Put the JSon information in String probably work but not optimized at all
+
+           for(Map.Entry<?,?> test : map.entrySet()){
+               if(test.getKey() == "url"){
+                   url = (String) test.getValue();
+               }else if(test.getKey() == "password"){
+                   password = (String) test.getValue();
+               }else if(test.getKey() == "dbName"){
+                   dbName = (String) test.getValue();
+               }else if(test.getKey() == "login"){
+                   login = (String) test.getValue();
+               }
+           }
+
 
             //Print the Map entries
 
