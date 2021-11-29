f0c51ab3998efbc1cc894af1011c9e9103894d3a
Noah <noah.remerand@etu.univ-tours.fr>
2021-11-29 10:36:13 +0100

    Working Json parser and reader

--- fr.project/src/JSONFileReading/JSONParser.java
+++ fr.project/src/JSONFileReading/JSONParser.java
@@ -1,6 +1,5 @@
 package JSONFileReading;
 
-
 import com.google.gson.Gson;
 
 import java.io.Reader;
@@ -8,13 +7,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 
+
 public class JSONParser {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        String url,password, dbName, login;
 
-        //Not usable for now need to finish it at one point though.
+        //String initialization
+        String url = null,password = null, dbName = null, login = null;
 
         try{
 
@@ -22,25 +22,27 @@ public class JSONParser {
             Gson gson = new Gson();
 
             //Create a Reader
-            Reader reader = Files.newBufferedReader(Path.of("/bdd.json"));
+            Reader reader = Files.newBufferedReader(Path.of("./bdd.json"));
 
             //Mapping JSON File
             Map<?, ?> map = gson.fromJson(reader, Map.class);
 
             //Put the JSon information in String probably work but not optimized at all
 
-           for(Map.Entry<?,?> test : map.entrySet()){
-               if(test.getKey() == "url"){
-                   url = (String) test.getValue();
-               }else if(test.getKey() == "password"){
-                   password = (String) test.getValue();
-               }else if(test.getKey() == "dbName"){
-                   dbName = (String) test.getValue();
-               }else if(test.getKey() == "login"){
-                   login = (String) test.getValue();
-               }
-           }
+            for(Map.Entry<?,?> test : map.entrySet()){
+                if(test.getKey().equals("url")){
+                    url = (String) test.getValue();
+                }else if(test.getKey().equals("password")){
+                    password = (String) test.getValue();
+                }else if(test.getKey().equals("dbName")){
+                    dbName = (String) test.getValue();
+                }else if(test.getKey().equals("login")){
+                    login = (String) test.getValue();
+                }
+            }
 
+            //Test print String
+            System.out.println(url + " " + password + " " + dbName + " " + login);
 
             //Print the Map entries
 
