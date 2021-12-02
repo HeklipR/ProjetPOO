dea4ef08491359edf912c224ecd9c493f931cd32
sikayoshi <yoshisika@yahoo.fr>
2021-12-02 16:23:16 +0100

    de la merde

---
+++ fr.project/src/Personne/test4.java
@@ -0,0 +1,18 @@
+package Personne;
+
+public class test4 {
+
+    private String s=null;
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public test4(String s) {
+        this.s = s;
+    }
+}
