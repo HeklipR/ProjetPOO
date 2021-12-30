aa33d5d50ab1f51bfaccc6daadf1ab7ab51bb814
Yvelin <yvelin.gerbeaud@etu.univ-tours.fr>
2021-12-30 17:55:44 +0100

    Création des classes pour le fxml

---
+++ fr.project/src/FXML/MainAncienEtudiant.java
@@ -0,0 +1,26 @@
+package FXML;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainAncienEtudiant extends Application {
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("recherche Ancien Etudiant.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("Ancien étudiants");
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {launch(args);
+    }
+}
